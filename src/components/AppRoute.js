ebdced5dbcc5d7bcec4cbfbbb9e7cf4f4b4ec978
fishkatsu <oeykevinwijaya@gmail.com>
2023-12-05 22:05:59 +1100

    updating path /photo

--- src/components/AppRoute.js
+++ src/components/AppRoute.js
@@ -5,15 +5,15 @@ import Gallery from "../pages/gallery";
 import About from "../pages/about";
 
 const AppRoutes = () => {
-    return (
-        <BrowserRouter basename="/oeykevinwijaya">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/gallery" element={<Gallery />} />
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </BrowserRouter>
-    );
+      return (
+            <BrowserRouter basename="/photo">
+                  <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/gallery" element={<Gallery />} />
+                        <Route path="/about" element={<About />} />
+                  </Routes>
+            </BrowserRouter>
+      );
 };
 
 export default AppRoutes;
